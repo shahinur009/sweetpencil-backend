605ae57b515cc54ced0c2c9b547c38958804ee20
BP Devs <bpdevs1037@gmail.com>
2025-03-09 23:26:15 +0600

    updated

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ypdnj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 // API version
 const client = new MongoClient(uri, {
   serverApi: {
